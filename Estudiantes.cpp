b50fd6adc5bdb8af53a3aecd7466e03a12754e56
bardaya <159058233+bardaya@users.noreply.github.com>
2024-03-04 13:38:20 -0400

    Add files via upload

---
+++ Estudiantes.cpp
@@ -0,0 +1,18 @@
+#include "Estudiantes.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Estudiante::Estudiante(); {
+
+}
+Estudiante::~Estudiante(); {
+
+}
+void Estudiante::set_nombre(string _n) {
+	nombre = _n;
+}
+string Estudiante::get_nombre() {
+	return nombre;
+}
