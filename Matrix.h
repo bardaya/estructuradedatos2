d9d1ee2f598f309ab2fcc983f14bf6ca04de5e81
bardaya <159058233+bardaya@users.noreply.github.com>
2024-03-18 13:35:56 -0400

    Add files via upload

---
+++ Matrix.h
@@ -0,0 +1,21 @@
+#pragma once
+#define MAX 20
+class Matrix
+{
+private:
+	int M[MAX][MAX];
+	int fil;
+	int col;
+public:
+	Matrix();
+	~Matrix();
+	void set_fil(int _fil);
+	int get_fil();
+	void set_col(int _col);
+	int get_col();
+	void mostrarmat(int MAT[MAX][MAX], int m, int n);
+	void cargarmat(int MAT[MAX][MAX], int m, int n);
+	float promedio(int MAT[MAX][MAX], int m, int n);
+	int mayor(int MAT[MAX][MAX], int m, int n);
+};
+
